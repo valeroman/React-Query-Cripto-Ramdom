9d894086638e08a9f59d49e7cc002afd569c32b5
Roman Valero <valeroman@gmail.com>
2022-11-10 16:41:43 -0500

    Primer commit

---
+++ src/hooks/useRandom.tsx
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query";
+
+
+const getRandomNumberFromApi = async():Promise<number> => {
+    const res = await fetch('https://www.random.org/integers/?num=1&min=1&max=500&col=1&base=10&format=plain&rnd=new');
+    const numberString = await res.text();
+  
+    // throw new Error('Auxilio!!!');
+    return +numberString;
+}
+  
+
+export const useRandom = () => {
+
+    // [] => indicar como queremos que se maneje nuestro cache
+    // () => función que voy a utilizar para cargar la información, siempre tiene que regresar un valor
+    const query = useQuery(
+        ['randomNumber'],
+        getRandomNumberFromApi
+    );
+
+    return query;
+}
